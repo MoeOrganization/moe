08ea8a3aab7e0f64c90b66a03881807197b6581c
Stevan Little <stevan.little@iinteractive.com>
2013-01-06 14:29:56 -0500

    moving the errors into the MoeRuntime object

--- src/main/scala/org/moe/Moe.scala
+++ src/main/scala/org/moe/Moe.scala
@@ -2,22 +2,6 @@ package org.moe
 
 object Moe {
 
-    val VERSION   = 0.01;
-    val AUTHORITY = "cpan:STEVAN";
-
-    object Errors {
-        class ValueNotFound         ( msg : String ) extends Exception( msg )
-        class InstanceValueNotFound ( msg : String ) extends ValueNotFound( msg )
-        class MethodNotFound        ( msg : String ) extends ValueNotFound( msg )
-        class AttributeNotFound     ( msg : String ) extends ValueNotFound( msg )
-
-        class UndefinedValue        ( msg : String ) extends Exception( msg )
-        class UndefinedMethod       ( msg : String ) extends UndefinedValue( msg )
-
-        class MissingValue          ( msg : String ) extends Exception( msg )
-        class MissingClass          ( msg : String ) extends MissingValue( msg )
-    }
-
     def main ( args: Array[String] ): Unit = {
         println( "Hello World" )
     }    
