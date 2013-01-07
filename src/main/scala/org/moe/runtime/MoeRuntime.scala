d13f38ab4478add4a9f182b2ee754383b5267856
Stevan Little <stevan.little@iinteractive.com>
2013-01-06 19:55:48 -0500

    some new exceptions

--- src/main/scala/org/moe/runtime/MoeRuntime.scala
+++ src/main/scala/org/moe/runtime/MoeRuntime.scala
@@ -3,6 +3,9 @@ package org.moe.runtime
 object Runtime {
 
     object Errors {
+        class NotAllowed            ( msg : String ) extends Exception( msg )
+        class MethodNotAllowed      ( msg : String ) extends NotAllowed( msg )
+
         class ValueNotFound         ( msg : String ) extends Exception( msg )
         class InstanceValueNotFound ( msg : String ) extends ValueNotFound( msg )
         class MethodNotFound        ( msg : String ) extends ValueNotFound( msg )
