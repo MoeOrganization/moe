5098d8b371d2755e32345265a147c595dced5b91
Stevan Little <stevan.little@iinteractive.com>
2013-01-08 18:03:39 -0500

    more tests for new evals

--- src/test/scala/org/moe/interpreter/InterpreterTestSuite.scala
+++ src/test/scala/org/moe/interpreter/InterpreterTestSuite.scala
@@ -31,7 +31,92 @@ class InterpreterTestSuite extends FunSuite with BeforeAndAfter {
         )
 
         val result = Interpreter.eval( Runtime.getRootEnv(), ast )
-        assert( result.asInstanceOf[ MoeIntObject ].getNativeValue() ===  10 )
+        assert( result.asInstanceOf[ MoeIntObject ].getNativeValue() === 10 )
+    }  
+
+    test("... basic test with Float") {
+        val ast = CompilationUnitNode(
+            ScopeNode(
+                StatementsNode(
+                    List( FloatLiteralNode( 10.5 ) )
+                )
+            )
+        )
+
+        val result = Interpreter.eval( Runtime.getRootEnv(), ast )
+        assert( result.asInstanceOf[ MoeFloatObject ].getNativeValue() === 10.5 )
+    } 
+
+    test("... basic test with String") {
+        val ast = CompilationUnitNode(
+            ScopeNode(
+                StatementsNode(
+                    List( StringLiteralNode( "HELLO" ) )
+                )
+            )
+        )
+
+        val result = Interpreter.eval( Runtime.getRootEnv(), ast )
+        assert( result.asInstanceOf[ MoeStringObject ].getNativeValue() === "HELLO" )
+    }     
+
+    // some simple logical operators
+
+    test("... basic test with And") {
+        val ast = CompilationUnitNode(
+            ScopeNode(
+                StatementsNode(
+                    List( 
+                        AndNode(
+                            BooleanLiteralNode( true ),
+                            BooleanLiteralNode( false )
+                        )
+                    )
+                )
+            )
+        )
+
+        val result = Interpreter.eval( Runtime.getRootEnv(), ast )
+        assert( result.asInstanceOf[ MoeBooleanObject ].getNativeValue() === false )
+    } 
+
+    test("... basic test with nested And") {
+        val ast = CompilationUnitNode(
+            ScopeNode(
+                StatementsNode(
+                    List( 
+                        AndNode(
+                            BooleanLiteralNode( true ),
+                            AndNode(
+                                BooleanLiteralNode( true ),
+                                IntLiteralNode( 100 )
+                            )
+                        )
+                    )
+                )
+            )
+        )
+
+        val result = Interpreter.eval( Runtime.getRootEnv(), ast )
+        assert( result.asInstanceOf[ MoeIntObject ].getNativeValue() === 100 )
     }    
+
+    test("... basic test with Or") {
+        val ast = CompilationUnitNode(
+            ScopeNode(
+                StatementsNode(
+                    List( 
+                        OrNode(
+                            BooleanLiteralNode( true ),
+                            BooleanLiteralNode( false )
+                        )
+                    )
+                )
+            )
+        )
+
+        val result = Interpreter.eval( Runtime.getRootEnv(), ast )
+        assert( result.asInstanceOf[ MoeBooleanObject ].getNativeValue() === true )
+    } 
 
 }
