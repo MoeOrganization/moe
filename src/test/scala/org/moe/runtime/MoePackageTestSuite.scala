1bd6d6bc0c5dddea67c9afb61730d056eee2db78
Stevan Little <stevan.little@iinteractive.com>
2013-01-09 21:14:23 -0500

    expanding the package tests a little

--- src/test/scala/org/moe/runtime/MoePackageTestSuite.scala
+++ src/test/scala/org/moe/runtime/MoePackageTestSuite.scala
@@ -5,9 +5,29 @@ import org.scalatest.BeforeAndAfter
 
 class MoePackageTestSuite extends FunSuite with BeforeAndAfter {
 
+    var pkg : MoePackage = _
+
+    before {
+        pkg = new MoePackage( "main", new MoeEnvironment() )
+    }
+
     test("... basic package") {
-        var pkg = new MoePackage( "main", new MoeEnvironment() )
         assert( pkg.getName() === "main" )
+    } 
+
+    test("... basic package w/ subroutine") {
+        pkg.addSubroutine( new MoeSubroutine( "ident", ( args ) => args(0) ) )
+        assert( pkg.hasSubroutine( "ident" ) )
     }  
+
+    test("... basic package w/ class") {
+        pkg.addClass( new MoeClass( "Foo" ) )
+        assert( pkg.hasClass( "Foo" ) )
+    }  
+
+    test("... basic package w/ sub-package") {
+        pkg.addSubPackage( new MoePackage( "Foo", new MoeEnvironment() ) )
+        assert( pkg.hasSubPackage( "Foo" ) )
+    } 
     
 }
