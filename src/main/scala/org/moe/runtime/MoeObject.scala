7341fc356d5dd4576a2e1834e7371c0fe69c7784
Cory G Watson <gphat@onemogin.com>
2013-01-13 17:05:24 -0600

    Clean up MoeObject further and add documentation.
    * Use the implicit constructor with a default to reduce complexity
    * Add scaladoc to methods and class
    * Rename some variables to be more descriptive
    * Use named parameters in methods, since this is one of those libraries
      that needs to be read by others and it will change often. Future
      proofing!

--- src/main/scala/org/moe/runtime/MoeObject.scala
+++ src/main/scala/org/moe/runtime/MoeObject.scala
@@ -1,33 +1,73 @@
 package org.moe.runtime
 
-class MoeObject {
+/** An object!
+ * @param associatedClass The class to associated with this object.
+ */
+class MoeObject(associatedClass: Option[MoeClass] = None) {
 
   private val id: Int = System.identityHashCode(this)
 
-  private var klass: Option[MoeClass] = None
+  private var klass: Option[MoeClass] = associatedClass
 
-  def this (k: Option[MoeClass]) = {
-    this()
-    klass = k
-  }
-
+  /**
+   * Returns the ID of this class.
+   */
   def getID: Int = id
+
+  /**
+   * Returns the class associted with this object, if there is one.
+   */
   def getAssociatedClass: Option[MoeClass] = klass
+
+  /**
+   * Returns true if a class is associated with this object.
+   */
   def hasAssociatedClass: Boolean = klass.isDefined
 
-  def setAssociatedClass(k: Option[MoeClass]) = klass = k
+  /**
+   * Set the class that is associated with this object.
+   *
+   * @param c The class to associate with this object.
+   */
+  def setAssociatedClass(c: Option[MoeClass]) = klass = c
 
-  def callMethod(method: String): MoeObject = callMethod(method, List())
-  def callMethod(method: String, args: List[MoeObject]): MoeObject = {
-    klass.map({ k => k.getMethod(method).execute(this, args) }).getOrElse(
+  /**
+   * Invoke the named method.
+   *
+   * @param name The name of the method
+   */
+  def callMethod(name: String): MoeObject = callMethod(name = name, args = List())
+
+  /**
+   * Invoke the named method with the supplied arguments.
+   *
+   * @param name The name of the method
+   * @param args The list of arguments to provide to the method
+   */
+  def callMethod(name: String, args: List[MoeObject]): MoeObject = {
+    klass.map({ k => k.getMethod(name).execute(this, args) }).getOrElse(
       throw new Runtime.Errors.MissingClass(toString)
     )
   }
 
+  /**
+   * Returns true.
+   */
   def isTrue: Boolean = !isFalse
+
+  /**
+   * Returns false.
+   */
   def isFalse: Boolean = false
+
+  /**
+   * Returns false
+   */
   def isUndef: Boolean = false
 
+  /**
+   * Returns a string representation of this object.
+   */
   override def toString: String = {
     "{ #instance(" + id + ")" + klass.map({ k => k.toString }).getOrElse("") + "}"
   }
