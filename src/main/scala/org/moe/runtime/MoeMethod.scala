565b049308fb990a1ef65717d1fea11e22506cc0
Stevan Little <stevan.little@iinteractive.com>
2013-01-14 22:08:20 -0500

    some style fixup and some docs

--- src/main/scala/org/moe/runtime/MoeMethod.scala
+++ src/main/scala/org/moe/runtime/MoeMethod.scala
@@ -1,17 +1,27 @@
 package org.moe.runtime
 
 class MoeMethod (
-  private val name: String,
-  private val body: (MoeObject, List[MoeObject]) => MoeObject)
-  extends MoeObject {
+    private val name: String,
+    private val body: (MoeObject, List[MoeObject]) => MoeObject
+  ) extends MoeObject {
 
   def this (name : String) = {
     // yadda yadda yadda
     this(name, (inv, args) => throw new Runtime.Errors.UndefinedMethod(name))
   }
 
+  /**
+   * Returns the name of this method
+   */
   def getName: String = name
+
+  /**
+   * Returns the executable body of this method
+   */
   def getBody: (MoeObject, List[MoeObject]) => MoeObject = body
 
+  /**
+   * Executes the body of this method passing in a list of arguments
+   */
   def execute(invocant: MoeObject, args: List[MoeObject]): MoeObject = body(invocant, args)
 }
--- src/main/scala/org/moe/runtime/MoeSubroutine.scala
+++ src/main/scala/org/moe/runtime/MoeSubroutine.scala
@@ -1,17 +1,27 @@
 package org.moe.runtime
 
 class MoeSubroutine (
-  private val name: String,
-  private val body: (List[MoeObject]) => MoeObject)
-  extends MoeObject {
+    private val name: String,
+    private val body: (List[MoeObject]) => MoeObject
+  ) extends MoeObject {
 
-  def this (name : String) = {
+  def this (name: String) = {
     // yadda yadda yadda
     this(name, (args) => throw new Runtime.Errors.UndefinedSubroutine(name))
   }
 
+  /**
+   * Returns the name of this subroutine
+   */
   def getName: String = name
+
+  /**
+   * Returns the executable body of this subroutine
+   */
   def getBody: (List[MoeObject]) => MoeObject = body
 
+  /**
+   * Executes the body of this subroutine passing in a list of arguments
+   */
   def execute (args : List[MoeObject]): MoeObject = body(args)
 }
