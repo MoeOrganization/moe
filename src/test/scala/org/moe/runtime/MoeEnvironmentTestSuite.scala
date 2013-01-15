982a4591548890a31a730ed3c132ebf04fcb3713
Cory G Watson <gphat@onemogin.com>
2013-01-15 00:36:27 -0600

    Verify that set works on non-root environment both changing and overriding (local-style).

--- src/test/scala/org/moe/runtime/MoeEnvironmentTestSuite.scala
+++ src/test/scala/org/moe/runtime/MoeEnvironmentTestSuite.scala
@@ -56,4 +56,68 @@ class MoeEnvironmentTestSuite extends FunSuite with BeforeAndAfter {
     assert(ex.getMessage === "$baz")
   }
 
+  test("... child changes parent") {
+    val child = new MoeEnvironment(env)
+
+    // Make sure the child knows it's a child
+    assert(!child.isRoot)
+
+    val c001 = new MoeObject()
+    val c002 = new MoeObject()
+
+    // Add $foo to the parent
+    env.create("$foo", c001)
+    // Verify env knows it has a $foo
+    assert(env.has("$foo"))
+    // And that the child has it as well
+    assert(child.has("$foo"))
+
+    // Verify that the child reaches into the parent for $foo
+    assert(child.get("$foo") === c001)
+
+    // Verify that the child & parent work for a missing value
+    val ex = intercept[Runtime.Errors.UndefinedValue] {
+      env.set("$baz", c001)
+    }
+    assert(ex.getMessage === "$baz")
+
+    // Set $foo in the child, changing the parent
+    child.set("$foo", c002)
+    // Check the child's first
+    assert(child.get("$foo") != c001)
+    assert(child.get("$foo") === c002)
+    // Now the parent
+    assert(env.get("$foo") != c001)
+    assert(env.get("$foo") === c002)
+  }
+
+  test("... child overrides parent") {
+    val child = new MoeEnvironment(env)
+
+    // Make sure the child knows it's a child
+    assert(!child.isRoot)
+
+    val c001 = new MoeObject()
+    val c002 = new MoeObject()
+
+    // Add $foo to the parent
+    env.create("$foo", c001)
+    // Verify env knows it has a $foo
+    assert(env.has("$foo"))
+    // And that the child has it as well
+    assert(child.has("$foo"))
+
+    // Verify that the child reaches into the parent for $foo
+    assert(child.get("$foo") === c001)
+
+    // Set $foo in the child, leaving the parent
+    child.create("$foo", c002)
+    // Check the child's first
+    assert(child.get("$foo") != c001)
+    assert(child.get("$foo") === c002)
+    // Now the parent
+    assert(env.get("$foo") != c002)
+    assert(env.get("$foo") === c001)
+  }
+
 }
