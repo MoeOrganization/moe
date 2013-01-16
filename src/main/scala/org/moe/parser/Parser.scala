efbe3adbd2dd3cda52189c62026d0f1e6d60283a
Jason May <jason.a.may@gmail.com>
2013-01-16 12:08:46 -0500

    Pass in a regex instead of a string for consistency
    It appears that using a string is what caused the parser method to be
    type Any.

--- src/main/scala/org/moe/parser/Parser.scala
+++ src/main/scala/org/moe/parser/Parser.scala
@@ -5,14 +5,18 @@ import scala.util.parsing.combinator._
 import org.moe.ast._
  
 object Parser extends RegexParsers {
+
+  // Numeric literals
   def intNumber   = """[1-9][0-9_]+""".r ^^ { n => IntLiteralNode(n.replace("_", "").toInt) }
   def octIntNumber = """0[0-9_]+""".r ^^ { n => IntLiteralNode(Integer.parseInt(n.replace("_", ""), 8)) }
   def hexIntNumber = """0x[0-9A-Fa-f_]+""".r ^^ { n => IntLiteralNode(Integer.parseInt(n.replace("_", "")
                                                                       .replace("0x", "").toUpperCase, 16)) }
   def binIntNumber = """0b[01_]+""".r ^^ { n => IntLiteralNode(Integer.parseInt(n.replace("_", "").replace("0b", ""), 2)) }
   def floatNumber = """[0-9_]*\.[0-9_]+""".r ^^ { n => FloatLiteralNode(n.replace("_", "").toDouble) }
-  def constTrue  : Parser[AST] = "true" ^^ { _ => BooleanLiteralNode(true) }
-  def constFalse : Parser[AST] = "false" ^^ { _ => BooleanLiteralNode(false) }
+
+  // Boolean literals
+  def constTrue = "true".r ^^ { _ => BooleanLiteralNode(true) }
+  def constFalse = "false".r ^^ { _ => BooleanLiteralNode(false) }
 
   def literal = floatNumber | intNumber | octIntNumber | hexIntNumber | binIntNumber | constTrue | constFalse
 
