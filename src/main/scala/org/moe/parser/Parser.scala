189cb942eaf21dfaf29f4af34a77be0f5a0ee50d
Brock Wilcox <awwaiid@thelackthereof.org>
2013-01-16 08:10:15 -0500

    Expand to match some literals I don't know why I had to force the types for constTrue/False :(

--- src/main/scala/org/moe/parser/Parser.scala
+++ src/main/scala/org/moe/parser/Parser.scala
@@ -7,9 +7,13 @@ import org.moe.ast._
 object Parser extends RegexParsers {
   def intNumber   = """[0-9]+""".r ^^ { n => IntLiteralNode(n.toInt) }
   def floatNumber = """[0-9]+(\.[0-9]+)?""".r ^^ { n => FloatLiteralNode(n.toDouble) }
+  def constTrue  : Parser[AST] = "true" ^^ { _ => BooleanLiteralNode(true) }
+  def constFalse : Parser[AST] = "false" ^^ { _ => BooleanLiteralNode(false) }
+
+  def literal = intNumber | floatNumber | constTrue | constFalse
 
   // Parser wrapper -- indicates the start node
-  def parseStuff(input: String): AST = parseAll(intNumber, input) match {
+  def parseStuff(input: String): AST = parseAll(literal, input) match {
     case Success(result, _) => result
     case failure : NoSuccess => scala.sys.error(failure.msg)
   }
