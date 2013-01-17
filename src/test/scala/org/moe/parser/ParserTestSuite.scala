a3eec412711e7d7bb28afd32b54c5e2a24cab2d2
Jason May <jason.a.may@gmail.com>
2013-01-17 13:05:41 -0500

    Factor common boilerplate into a private method for parser tests

--- src/test/scala/org/moe/parser/ParserTestSuite.scala
+++ src/test/scala/org/moe/parser/ParserTestSuite.scala
@@ -13,119 +13,105 @@ class ParserTestSuite extends FunSuite with BeforeAndAfter {
   private def basicAST(nodes: List[AST]) =
     CompilationUnitNode(ScopeNode(StatementsNode(nodes)))
 
+  private def interpretCode(code: String): MoeObject = {
+    val ast = basicAST(List(Parser.parseStuff(code)))
+    Interpreter.eval(Runtime.getRootEnv, ast)
+  }
+
   test("... basic test with an int") {
-    val ast = basicAST(List(Parser.parseStuff("123")))
-    val result = Interpreter.eval(Runtime.getRootEnv, ast)
+    val result = interpretCode("123")
     assert(result.asInstanceOf[MoeIntObject].getNativeValue === 123)
   }
 
   test("... basic test with an int - embedded underscore") {
-    val ast = basicAST(List(Parser.parseStuff("123_456")))
-    val result = Interpreter.eval(Runtime.getRootEnv, ast)
+    val result = interpretCode("123_456")
     assert(result.asInstanceOf[MoeIntObject].getNativeValue === 123456)
   }
 
   test("... basic test with an octal int") {
-    val ast = basicAST(List(Parser.parseStuff("0123")))
-    val result = Interpreter.eval(Runtime.getRootEnv, ast)
+    val result = interpretCode("0123")
     assert(result.asInstanceOf[MoeIntObject].getNativeValue === 83)
   }
 
   test("... basic test with an octal int - embedded underscore") {
-    val ast = basicAST(List(Parser.parseStuff("0123_456")))
-    val result = Interpreter.eval(Runtime.getRootEnv, ast)
+    val result = interpretCode("0123_456")
     assert(result.asInstanceOf[MoeIntObject].getNativeValue === 42798)
   }
 
   test("... basic test with an hexadecimal int") {
-    val ast = basicAST(List(Parser.parseStuff("0x123")))
-    val result = Interpreter.eval(Runtime.getRootEnv, ast)
+    val result = interpretCode("0x123")
     assert(result.asInstanceOf[MoeIntObject].getNativeValue === 0x123)
   }
 
   test("... basic test with an hexadecimal int - 2") {
-    val ast = basicAST(List(Parser.parseStuff("0xFFFF")))
-    val result = Interpreter.eval(Runtime.getRootEnv, ast)
+    val result = interpretCode("0xFFFF")
     assert(result.asInstanceOf[MoeIntObject].getNativeValue === 0xFFFF)
   }
 
   test("... basic test with an hexadecimal int - embedded underscore") {
-    val ast = basicAST(List(Parser.parseStuff("0x123_456")))
-    val result = Interpreter.eval(Runtime.getRootEnv, ast)
+    val result = interpretCode("0x123_456")
     assert(result.asInstanceOf[MoeIntObject].getNativeValue === 0x123456)
   }
 
   test("... basic test with an hexadecimal int - embedded underscore - 2") {
-    val ast = basicAST(List(Parser.parseStuff("0xAB_CDEF")))
-    val result = Interpreter.eval(Runtime.getRootEnv, ast)
+    val result = interpretCode("0xAB_CDEF")
     assert(result.asInstanceOf[MoeIntObject].getNativeValue === 0xABCDEF)
   }
 
   test("... basic test with an binary int literal") {
-    val ast = basicAST(List(Parser.parseStuff("0b10110")))
-    val result = Interpreter.eval(Runtime.getRootEnv, ast)
+    val result = interpretCode("0b10110")
     assert(result.asInstanceOf[MoeIntObject].getNativeValue === 22)
   }
 
   test("... basic test with an binary int literal - embedded underscore") {
-    val ast = basicAST(List(Parser.parseStuff("0b1011_0110")))
-    val result = Interpreter.eval(Runtime.getRootEnv, ast)
+    val result = interpretCode("0b1011_0110")
     assert(result.asInstanceOf[MoeIntObject].getNativeValue === 182)
   }
 
   test("... basic test with a float") {
-    val ast = basicAST(List(Parser.parseStuff("123.5")))
-    val result = Interpreter.eval(Runtime.getRootEnv, ast)
+    val result = interpretCode("123.5")
     assert(result.asInstanceOf[MoeFloatObject].getNativeValue === 123.5)
   }
 
   test("... basic test with a float - 2") {
-    val ast = basicAST(List(Parser.parseStuff(".5678")))
-    val result = Interpreter.eval(Runtime.getRootEnv, ast)
+    val result = interpretCode(".5678")
     assert(result.asInstanceOf[MoeFloatObject].getNativeValue === .5678)
   }
 
   test("... basic test with a float - embedded underscore") {
-    val ast = basicAST(List(Parser.parseStuff("123_456.56_789")))
-    val result = Interpreter.eval(Runtime.getRootEnv, ast)
+    val result = interpretCode("123_456.56_789")
     assert(result.asInstanceOf[MoeFloatObject].getNativeValue === 123456.56789)
   }
 
   test("... basic test with a true") {
-    val ast = basicAST(List(Parser.parseStuff("true")))
-    val result = Interpreter.eval(Runtime.getRootEnv, ast)
+    val result = interpretCode("true")
     assert(result.asInstanceOf[MoeBooleanObject].getNativeValue === true)
   }
 
   test("... basic test with a false") {
-    val ast = basicAST(List(Parser.parseStuff("false")))
-    val result = Interpreter.eval(Runtime.getRootEnv, ast)
+    val result = interpretCode("false")
     assert(result.asInstanceOf[MoeBooleanObject].getNativeValue === false)
   }
 
   test("... basic test with a simple double-quoted string") {
-    val ast = basicAST(List(Parser.parseStuff("\"hello world\"")))
-    val result = Interpreter.eval(Runtime.getRootEnv, ast)
+    val result = interpretCode("\"hello world\"")
     assert(result.asInstanceOf[MoeStringObject].getNativeValue === "hello world")
   }
 
   test("... basic test with a simple single-quoted string") {
-    val ast = basicAST(List(Parser.parseStuff("'hello world'")))
-    val result = Interpreter.eval(Runtime.getRootEnv, ast)
+    val result = interpretCode("'hello world'")
     assert(result.asInstanceOf[MoeStringObject].getNativeValue === "hello world")
   }
 
   test("... basic test with a one-element arrayref") {
-    val ast = basicAST(List(Parser.parseStuff("[42]")))
-    val result = Interpreter.eval(Runtime.getRootEnv, ast)
+    val result = interpretCode("[42]")
 
     val listElement = result.asInstanceOf[MoeArrayObject].getNativeValue(0)
     assert(listElement.asInstanceOf[MoeIntObject].getNativeValue === 42);
   }
 
   test("... basic test with a four-element arrayref") {
-    val ast = basicAST(List(Parser.parseStuff("""[42, 'jason', "may", true]""")))
-    val result = Interpreter.eval(Runtime.getRootEnv, ast)
+    val result = interpretCode("""[42, 'jason', "may", true]""")
     val elems = result.asInstanceOf[MoeArrayObject].getNativeValue
 
     assert(elems(0).asInstanceOf[MoeIntObject].getNativeValue === 42);
@@ -136,8 +122,7 @@ class ParserTestSuite extends FunSuite with BeforeAndAfter {
   }
 
   test("... basic test with nested arrayrefs") {
-    val ast = basicAST(List(Parser.parseStuff("""[42, ['jason', "may"], true]""")))
-    val result = Interpreter.eval(Runtime.getRootEnv, ast)
+    val result = interpretCode("""[42, ['jason', "may"], true]""")
     val elems = result.asInstanceOf[MoeArrayObject].getNativeValue
 
     assert(elems(0).asInstanceOf[MoeIntObject].getNativeValue === 42);
@@ -150,8 +135,7 @@ class ParserTestSuite extends FunSuite with BeforeAndAfter {
   }
 
   test("... basic test with arrayref containing a right-trailing list") {
-    val ast = basicAST(List(Parser.parseStuff("[42, true, ]")))
-    val result = Interpreter.eval(Runtime.getRootEnv, ast)
+    val result = interpretCode("[42, true, ]")
     val elems = result.asInstanceOf[MoeArrayObject].getNativeValue
 
     assert(elems(0).asInstanceOf[MoeIntObject].getNativeValue === 42);
@@ -159,8 +143,7 @@ class ParserTestSuite extends FunSuite with BeforeAndAfter {
   }
 
   test("... basic test with arrayref containing a left-trailing list") {
-    val ast = basicAST(List(Parser.parseStuff("[, 42, true]")))
-    val result = Interpreter.eval(Runtime.getRootEnv, ast)
+    val result = interpretCode("[, 42, true]")
     val elems = result.asInstanceOf[MoeArrayObject].getNativeValue
 
     assert(elems(0).asInstanceOf[MoeIntObject].getNativeValue === 42);
