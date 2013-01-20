d0e7e36d7481e211e142748b8feac7bc80c2284e
Jason May <jason.a.may@gmail.com>
2013-01-19 19:50:59 -0500

    Cleanup, including moving stuff into a util package

--- src/main/scala/org/moe/parser/Parser.scala
+++ src/main/scala/org/moe/parser/Parser.scala
@@ -4,50 +4,82 @@ import scala.util.parsing.combinator._
 // import scala.util.matching.Regex
 import org.moe.ast._
 
+import ParserUtils._
+
 object Parser extends RegexParsers {
 
   // Numeric literals
-  def intNumber: Parser[IntLiteralNode]   = """[1-9][0-9_]*""".r ^^ { n => IntLiteralNode(n.replace("_", "").toInt) }
-  def octIntNumber: Parser[IntLiteralNode] = """0[0-9_]+""".r ^^ { n => IntLiteralNode(Integer.parseInt(n.replace("_", ""), 8)) }
-  def hexIntNumber: Parser[IntLiteralNode] = """0x[0-9A-Fa-f_]+""".r ^^ { n => IntLiteralNode(Integer.parseInt(n.replace("_", "")
-                                                                      .replace("0x", "").toUpperCase, 16)) }
-  def binIntNumber: Parser[IntLiteralNode] = """0b[01_]+""".r ^^ { n => IntLiteralNode(Integer.parseInt(n.replace("_", "").replace("0b", ""), 2)) }
-  def floatNumber: Parser[FloatLiteralNode] = """[0-9_]*\.[0-9_]+""".r ^^ { n => FloatLiteralNode(n.replace("_", "").toDouble) }
+  def intNumber: Parser[IntLiteralNode]   =
+  """[1-9][0-9_]*""".r ^^ { n => IntLiteralNode(formatInt(n)) }
+  def octIntNumber: Parser[IntLiteralNode] =
+  """0[0-9_]+""".r ^^ { n => IntLiteralNode(formatOctal(n)) }
+  def hexIntNumber: Parser[IntLiteralNode] =
+    """0x[0-9A-Fa-f_]+""".r ^^ { n => IntLiteralNode(formatHex(n)) }
+  def binIntNumber: Parser[IntLiteralNode] =
+    """0b[01_]+""".r ^^ { n => IntLiteralNode(formatBin(n)) }
+  def floatNumber: Parser[FloatLiteralNode] =
+    """[0-9_]*\.[0-9_]+""".r ^^ { n => FloatLiteralNode(formatFloat(n)) }
 
   // Boolean literals
-  def constTrue: Parser[BooleanLiteralNode] = "true".r ^^^ BooleanLiteralNode(true)
-  def constFalse: Parser[BooleanLiteralNode] = "false".r ^^^ BooleanLiteralNode(false)
+  def constTrue: Parser[BooleanLiteralNode] =
+    "true".r ^^^ BooleanLiteralNode(true)
+  def constFalse: Parser[BooleanLiteralNode] =
+    "false".r ^^^ BooleanLiteralNode(false)
 
   // String literals
-  def doubleQuoteStringContent: Parser[StringLiteralNode] = """[^"]*""".r ^^ StringLiteralNode
-  def doubleQuoteString: Parser[StringLiteralNode] = "\"".r ~> doubleQuoteStringContent <~ "\"".r
+  def doubleQuoteStringContent: Parser[StringLiteralNode] =
+    """[^"]*""".r ^^ StringLiteralNode
+  def doubleQuoteString: Parser[StringLiteralNode] =
+    "\"".r ~> doubleQuoteStringContent <~ "\"".r
 
-  def singleQuoteStringContent: Parser[StringLiteralNode] = """[^']*""".r  ^^ StringLiteralNode
-  def singleQuoteString: Parser[StringLiteralNode] = "'".r ~> singleQuoteStringContent <~ "'".r
+  def singleQuoteStringContent: Parser[StringLiteralNode] =
+    """[^']*""".r  ^^ StringLiteralNode
+  def singleQuoteString: Parser[StringLiteralNode] =
+    "'".r ~> singleQuoteStringContent <~ "'".r
 
   def string: Parser[StringLiteralNode] = doubleQuoteString | singleQuoteString
 
-  def literal: Parser[AST] = floatNumber | intNumber | octIntNumber | hexIntNumber | binIntNumber | constTrue | constFalse | string
+  def literal: Parser[AST] = (
+      floatNumber
+    | intNumber
+    | octIntNumber
+    | hexIntNumber
+    | binIntNumber
+    | constTrue
+    | constFalse
+    | string
+  )
 
-  def expression: Parser[AST] = literal | arrayRef | hashRef
+  def expression: Parser[AST] = (
+      literal
+    | arrayRef
+    | hashRef
+  )
 
   // List stuff
   def list: Parser[List[AST]] = (",?".r ~> repsep(expression, ",") <~ ",?".r)
 
-  def arrayRef: Parser[ArrayLiteralNode] = """\[""".r ~> list <~ """\]""".r ^^ ArrayLiteralNode
+  def arrayRef: Parser[ArrayLiteralNode] =
+    """\[""".r ~> list <~ """\]""".r ^^ ArrayLiteralNode
 
-  def barehashKey: Parser[StringLiteralNode] = """[0-9\w_]*""".r ^^ StringLiteralNode
+  // Hash stuff
+  def barehashKey: Parser[StringLiteralNode] =
+    """[0-9\w_]*""".r ^^ StringLiteralNode
   def hashKey: Parser[StringLiteralNode] = barehashKey | string
   // FIXME should be able to do <~ "=>".r ~> but couldn't fiddle with it enough
-  def pair: Parser[PairLiteralNode] = hashKey ~ "=>".r ~ expression ^^ { case a ~ b ~ c => PairLiteralNode(a, c) }
-  def hashContent: Parser[List[PairLiteralNode]] = repsep(pair, ",")
-  def hashRef: Parser[HashLiteralNode] = """\{""".r ~> hashContent <~ """\}""".r ^^ HashLiteralNode
+  def pair: Parser[PairLiteralNode] =
+    hashKey ~ "=>".r ~ expression ^^ { case a ~ b ~ c => PairLiteralNode(a, c) }
+  def hashContent: Parser[List[PairLiteralNode]] =
+    repsep(pair, ",")
+  def hashRef: Parser[HashLiteralNode] =
+    """\{""".r ~> hashContent <~ """\}""".r ^^ HashLiteralNode
 
   // awwaiid's experimental structures
 
   def loop: Parser[AST] = ifLoop // | forLoop | foreachLoop | whileLoop
 
-  def ifLoop: Parser[AST] = (("if" ~ "(") ~> expression) ~ (")" ~> block) ^^ { case a ~ b => IfNode(a,b) }
+  def ifLoop: Parser[AST] =
+    (("if" ~ "(") ~> expression) ~ (")" ~> block) ^^ { case a ~ b => IfNode(a,b) }
 
   // def forLoop = "for" ~ "(" ~> expression <~ ";" ~> expression <~ ";" ~> expression <~ ")" ~ block
   // def whileLoop = "if" ~ "(" ~> expression <~ ")" ~ block
@@ -63,16 +95,24 @@ object Parser extends RegexParsers {
   // FIXME I feel skipping over blank statements doesn't
   // portray the AST completely but I might just be splitting hairs
   def statementDelim: Parser[List[String]] = rep1(";")
-  def statements: Parser[List[AST]] = repsep(statement, statementDelim)
-  def blockContent: Parser[StatementsNode] = statements <~ statementDelim.? ^^ { StatementsNode(_) }
-  def block: Parser[StatementsNode] = """\{""".r ~> blockContent <~ """\}""".r
+  def statements: Parser[List[AST]] =
+    repsep(statement, statementDelim)
+  def blockContent: Parser[StatementsNode] =
+    statements <~ statementDelim.? ^^ { StatementsNode(_) }
+  def block: Parser[StatementsNode] =
+    """\{""".r ~> blockContent <~ """\}""".r
 
   def doBlock: Parser[StatementsNode] = "do".r ~> block
   def scopeBlock: Parser[ScopeNode] = block ^^ { ScopeNode(_) }
   // def packageBlock = "package" ~> packageName ~ block
   // def classBlock = "class" ~> className ~ block
 
-  def statement: Parser[AST] = loop | expression | doBlock | scopeBlock
+  def statement: Parser[AST] = (
+      loop
+    | expression
+    | doBlock
+    | scopeBlock
+  )
 
   // Parser wrapper -- indicates the start node
   def parseStuff(input: String): AST = parseAll(statement, input) match {
