cf02063f88ce9531b410d72b85821e219e2adca7
Kartik Thakore <thakore.kartik@gmail.com>
2013-01-25 15:08:27 -0500

    Added docs for moe object

--- src/main/scala/org/moe/Moe.scala
+++ src/main/scala/org/moe/Moe.scala
@@ -1,7 +1,9 @@
 package org.moe
 
+/** Moe object for [[mypackage.Moe]] instance */
 object Moe {
 
+    /** prints hello world */
     def main (args: Array[String]): Unit = {
         println("Hello World")
     }
