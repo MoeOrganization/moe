21b69de2c4151a6db562b73c008f5077153174f7
Stevan Little <stevan.little@iinteractive.com>
2013-01-29 13:53:01 -0500

    some minor formatting tweaks

--- src/main/scala/org/moe/parser/ParserUtils.scala
+++ src/main/scala/org/moe/parser/ParserUtils.scala
@@ -1,27 +1,12 @@
 package org.moe.parser
 
 object ParserUtils {
-  def normPerlInt(s: String): String = {
-    s.replace("_", "")
-  }
 
-  def formatInt(s: String): Int = {
-    normPerlInt(s).toInt
-  }
+  def normPerlInt (s: String): String = s.replace("_", "")
+  def formatInt   (s: String): Int    = normPerlInt(s).toInt
+  def formatFloat (s: String): Double = normPerlInt(s).toDouble
+  def formatOctal (s: String): Int    = Integer.parseInt( normPerlInt(s), 8 )
+  def formatHex   (s: String): Int    = Integer.parseInt( normPerlInt(s).substring(2).toUpperCase, 16 )
+  def formatBin   (s: String): Int    = Integer.parseInt( normPerlInt(s).substring(2).toUpperCase, 2  )
 
-  def formatFloat(s: String): Double = {
-    normPerlInt(s).toDouble
-  }
-
-  def formatOctal(s: String): Int = {
-    Integer.parseInt(normPerlInt(s), 8)
-  }
-
-  def formatHex(n: String): Int = {
-    Integer.parseInt(normPerlInt(n).substring(2).toUpperCase, 16)
-  }
-
-  def formatBin(n: String): Int = {
-    Integer.parseInt(normPerlInt(n).substring(2).toUpperCase, 2)
-  }
 }
