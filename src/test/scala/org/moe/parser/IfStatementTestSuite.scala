3912d8e087ceaa1dbf8114b6d04a42c2a3b6c64a
Stevan Little <stevan.little@iinteractive.com>
2013-01-29 21:42:54 -0500

    some more simple if statement tests

--- src/test/scala/org/moe/parser/IfStatementTestSuite.scala
+++ src/test/scala/org/moe/parser/IfStatementTestSuite.scala
@@ -10,9 +10,19 @@ import org.moe.parser._
 
 class IfStatementTestSuite extends FunSuite with BeforeAndAfter with ParserTestUtils {
 
+  test("... a simple if") {
+    val result = interpretCode("if (true) { 100 }")
+    assert(result.asInstanceOf[MoeIntObject].getNativeValue === 100)
+  }
+
   test("... a weird looking if") {
     val result = interpretCode("if(true) { 2; 7 }")
     assert(result.asInstanceOf[MoeIntObject].getNativeValue === 7)
+  }
+
+  test("... nested if") {
+    val result = interpretCode("if (true) { if ( true ) { 200 } }")
+    assert(result.asInstanceOf[MoeIntObject].getNativeValue === 200)
   }
 
 }
