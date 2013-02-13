d517e039ad0d69fcabebaf417d0a209c92df4b85
Kartik Thakore <thakore.kartik@gmail.com>
2013-02-13 16:32:08 -0500

    Added optional '-' to 0

--- src/main/scala/org/moe/parser/Literals.scala
+++ src/main/scala/org/moe/parser/Literals.scala
@@ -9,7 +9,7 @@ trait Literals extends Base {
 
   // Numeric literals
   def zeroNumber: Parser[IntLiteralNode]   =
-    """0""".r ^^ { n => IntLiteralNode(0) }
+    """-?0""".r ^^ { n => IntLiteralNode(0) }
   def intNumber: Parser[IntLiteralNode]   =
     """-?[1-9][0-9_]*""".r ^^ { n => IntLiteralNode(formatInt(n)) }
   def octIntNumber: Parser[IntLiteralNode] =
