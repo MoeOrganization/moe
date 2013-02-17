7318d3d7b9fa6388ca6c6ff0e9dfc10bee89fac5
Brock Wilcox <awwaiid@thelackthereof.org>
2013-02-16 22:45:51 -0500

    Update test to actually interpret and fix style

--- src/test/scala/org/moe/parser/SimpleExpressionTestSuite.scala
+++ src/test/scala/org/moe/parser/SimpleExpressionTestSuite.scala
@@ -2,22 +2,18 @@ package org.moe.parser
 
 import org.scalatest.FunSuite
 import org.scalatest.BeforeAndAfter
+import org.scalatest.matchers.ShouldMatchers
 
 import org.moe.runtime._
 import org.moe.interpreter._
 import org.moe.ast._
 import org.moe.parser._
 
-class SimpleExpressionTestSuite extends FunSuite with BeforeAndAfter with ParserTestUtils {
+class SimpleExpressionTestSuite extends FunSuite with BeforeAndAfter with ParserTestUtils with ShouldMatchers {
 
   test("... literal int addition") {
-    val ast = basicAST(Parser.parseStuff("2+2"))
-    println(Serializer.toJSON(ast))
-    // Interpreter.eval(runtime, runtime.getRootEnv, ast)
-  // }
-    // val result = interpretCode("2 + 2")
-    // assert(result.asInstanceOf[MoeIntObject].getNativeValue === 4)
+    val result = interpretCode("2+2")
+    result.asInstanceOf[MoeIntObject].getNativeValue should equal (4)
   }
-
 
 }
