5698ecc53430f026d7fa2d036d8b76fe04765ffa
Stevan Little <stevan.little@iinteractive.com>
2013-02-25 21:55:08 -0500

    make native objects mutable

--- src/main/scala/org/moe/runtime/nativeobjects/MoeNativeObject.scala
+++ src/main/scala/org/moe/runtime/nativeobjects/MoeNativeObject.scala
@@ -3,10 +3,11 @@ package org.moe.runtime
 import scala.util.{Try, Success, Failure}
 
 abstract class MoeNativeObject[A] (
-    private val value: A,
+    private var value: A,
     private var associatedClass: Option[MoeClass] = None
   ) extends MoeObject(associatedClass) {
 
   def getNativeValue: A = value
+  def setNativeValue(v: A) = value = v
 }
 
