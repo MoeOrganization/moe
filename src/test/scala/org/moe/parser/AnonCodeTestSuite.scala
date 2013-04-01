cba9d7ec5814cc0a94e33db8c25da8b7aeaf8137
Stevan Little <stevan.little@iinteractive.com>
2013-03-31 21:26:13 -0400

    test case for anon code

---
+++ src/test/scala/org/moe/parser/AnonCodeTestSuite.scala
@@ -0,0 +1,20 @@
+package org.moe.parser
+
+import org.scalatest.FunSuite
+import org.scalatest.BeforeAndAfter
+import org.scalatest.matchers.ShouldMatchers
+
+import org.moe.runtime._
+import org.moe.runtime.nativeobjects._
+import org.moe.interpreter._
+import org.moe.ast._
+import org.moe.parser._
+
+class AnonCodeTestSuite extends FunSuite with BeforeAndAfter with ParserTestUtils with ShouldMatchers {
+
+  test("... a basic anon-sub") {
+    val result = interpretCode("my &id = -> ($x) { $x }; &id->(10)")
+    result.unboxToInt.get should equal (10)
+  }
+
+}
