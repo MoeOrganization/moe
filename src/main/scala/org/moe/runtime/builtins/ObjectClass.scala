4ab6531d81263f16dc6633eebb617269aa42dad0
Stevan Little <stevan.little@iinteractive.com>
2013-04-02 15:11:06 -0400

    new should be in ObjectClass not ClassClass

--- src/main/scala/org/moe/runtime/builtins/ObjectClass.scala
+++ src/main/scala/org/moe/runtime/builtins/ObjectClass.scala
@@ -13,7 +13,29 @@ object ObjectClass {
       throw new MoeErrors.MoeStartupError("Could not find class Object")
     )
 
-    // MRO: Object
+    import r.NativeObjects._
+
+    def self(e: MoeEnvironment): MoeClass = e.getCurrentInvocantAs[MoeClass].getOrElse(
+      throw new MoeErrors.InvocantNotFound("Could not find invocant")
+    )
+
+
+    // MRO: Any, Object
+
+    objectClass.addMethod(
+      new MoeMethod(
+        "new",
+        new MoeSignature(),
+        env,
+        {
+          (e) => 
+            val c = self(e)
+            val i = c.newInstance.asInstanceOf[MoeOpaque]
+            c.collectAllAttributes.foreach(a => a._2.getDefault.map(i.setValue(a._1, _)))
+            i
+        }
+      )
+    )
 
     /**
      * List of Methods to support:
