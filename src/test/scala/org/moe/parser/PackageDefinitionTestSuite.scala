b4e9eff6590b3fce31c735839f478b8219348712
Stevan Little <stevan.little@iinteractive.com>
2013-04-04 21:21:22 -0400

    add some tests

--- src/test/scala/org/moe/parser/PackageDefinitionTestSuite.scala
+++ src/test/scala/org/moe/parser/PackageDefinitionTestSuite.scala
@@ -11,13 +11,22 @@ import org.moe.parser._
 class PackageDefinitionTestSuite extends FunSuite with BeforeAndAfter with ParserTestUtils {
 
   test("... a basic package block") {
-    val result = interpretCode("""
-      package foo::bar {
-      }
-    """)
-    result match {
-      case _: MoeObject => assert(true) // stub
-      case _ => assert(false)
-    }
+    val result = interpretCode("package Foo {}")
+    assert(result.asInstanceOf[MoePackage].getName === "Foo")
+  }
+
+  test("... simple package scoping") {
+    val result = interpretCode("package Foo { sub bar {'BAR'} } Foo::bar()")
+    assert(result.unboxToString.get === "BAR")
+  }
+
+  test("... nested package scoping") {
+    val result = interpretCode("package Foo { package Bar { sub baz {'BAZ'} } } Foo::Bar::baz()")
+    assert(result.unboxToString.get === "BAZ")
+  }
+
+  test("... complex package scoping") {
+    val result = interpretCode("package Foo { package Bar { sub baz {'BAZ'} sub bar {baz()} } } Foo::Bar::bar()")
+    assert(result.unboxToString.get === "BAZ")
   }
 }
