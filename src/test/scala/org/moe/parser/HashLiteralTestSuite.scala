8445b117e3b4130bf69d37579169dd51ec4efdbd
Prakash Kailasa <pk-github@kailasa.net>
2013-04-09 11:35:40 -0400

    added tests for array/hash elements as lvalues; changed tests to use ShouldMatchers

--- src/test/scala/org/moe/parser/HashLiteralTestSuite.scala
+++ src/test/scala/org/moe/parser/HashLiteralTestSuite.scala
@@ -2,27 +2,41 @@ package org.moe.parser
 
 import org.scalatest.FunSuite
 import org.scalatest.BeforeAndAfter
+import org.scalatest.matchers.ShouldMatchers
 
 import org.moe.runtime._
 import org.moe.interpreter._
 import org.moe.ast._
 import org.moe.parser._
 
-class HashLiteralTestSuite extends FunSuite with BeforeAndAfter with ParserTestUtils {
+class HashLiteralTestSuite extends FunSuite with BeforeAndAfter with ParserTestUtils with ShouldMatchers {
 
   test("... basic test with hashref") {
     val result = interpretCode("{foo => 'bar'}")
     val map = result.unboxToMap.get
     val key = map("foo")
-    assert(key.unboxToString.get === "bar")
+    key.unboxToString.get should equal ("bar")
   }
 
   test("... basic test with hashref of a string key") {
     val result = interpretCode("{'foo' => 'bar'}")
     val map = result.unboxToMap.get
     val key = map("foo")
-    assert(key.unboxToString.get === "bar")
+    key.unboxToString.get should equal ("bar")
   }
 
+  test("... basic test with hash element assignment") {
+    val result = interpretCode("my %h = {foo => 'bar'}; %h{'foo'} = 'baz'; %h")
+    val map = result.unboxToMap.get
+    val key = map("foo")
+    key.unboxToString.get should equal ("baz")
+  }
+
+  test("... basic test with hash element assignment -- non-existent element") {
+    val result = interpretCode("my %h = {foo => 'bar'}; %h{'baz'} = 'moe'; %h")
+    val map = result.unboxToMap.get
+    map("foo").unboxToString.get should equal ("bar")
+    map("baz").unboxToString.get should equal ("moe")
+  }
 
 }
