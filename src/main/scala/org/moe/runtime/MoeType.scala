735d3470118194fdd31310542cbac84c454e1633
Stevan Little <stevan.little@iinteractive.com>
2013-04-24 22:01:40 -0400

    need names

--- src/main/scala/org/moe/runtime/MoeType.scala
+++ src/main/scala/org/moe/runtime/MoeType.scala
@@ -2,8 +2,11 @@ package org.moe.runtime
 
 sealed abstract class MoeType (
     private val sigil: String,
+    private val name: String,
     private var associatedClass: Option[MoeClass]
   ) {
+
+  def getName  = name
   def getSigil = sigil
 
   def getAssociatedClass: Option[MoeClass] = associatedClass
@@ -11,9 +14,9 @@ sealed abstract class MoeType (
   def setAssociatedClass (c: Option[MoeClass]): Unit = associatedClass = c
 }
 
-case class MoeScalarType (c: Option[MoeClass] = None) extends MoeType("$", c)
-case class MoeArrayType  (c: Option[MoeClass] = None) extends MoeType("@", c)
-case class MoeHashType   (c: Option[MoeClass] = None) extends MoeType("%", c)
-case class MoeCodeType   (c: Option[MoeClass] = None) extends MoeType("&", c)
-case class MoeClassType  (c: Option[MoeClass] = None) extends MoeType("^", c)
+case class MoeScalarType (c: Option[MoeClass] = None) extends MoeType("$", "SCALAR", c)
+case class MoeArrayType  (c: Option[MoeClass] = None) extends MoeType("@", "ARRAY",  c)
+case class MoeHashType   (c: Option[MoeClass] = None) extends MoeType("%", "HASH",   c)
+case class MoeCodeType   (c: Option[MoeClass] = None) extends MoeType("&", "CODE",   c)
+case class MoeClassType  (c: Option[MoeClass] = None) extends MoeType("^", "CLASS",  c)
 
