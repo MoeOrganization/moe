edde401db8d5d7c6f62d66973077bf2e7c7c8b7f
Stevan Little <stevan.little@iinteractive.com>
2013-04-28 16:54:26 -0400

    add in the new AnyType

--- src/main/scala/org/moe/runtime/MoeType.scala
+++ src/main/scala/org/moe/runtime/MoeType.scala
@@ -1,7 +1,7 @@
 package org.moe.runtime
 
 object MoeType {
-    def checkType (n: String, t: MoeType)   : Boolean = n.startsWith(t.getSigil) 
+    def checkType (n: String, t: MoeType)   : Boolean = (t.isAnyType || n.startsWith(t.getSigil))
     def checkType (n: String, o: MoeObject) : Boolean = o.getAssociatedType.exists(checkType(n, _))
 }
 
@@ -14,11 +14,14 @@ sealed abstract class MoeType (
   def getName  = name
   def getSigil = sigil
 
+  def isAnyType = sigil == "*"
+
   def getAssociatedClass: Option[MoeClass] = associatedClass
   def hasAssociatedClass: Boolean = associatedClass.isDefined
   def setAssociatedClass (c: Option[MoeClass]): Unit = associatedClass = c
 }
 
+case class MoeAnyType    (c: Option[MoeClass] = None) extends MoeType("*", "ANY",    c)
 case class MoeScalarType (c: Option[MoeClass] = None) extends MoeType("$", "SCALAR", c)
 case class MoeArrayType  (c: Option[MoeClass] = None) extends MoeType("@", "ARRAY",  c)
 case class MoeHashType   (c: Option[MoeClass] = None) extends MoeType("%", "HASH",   c)
