aaf1117d05a53120349d099f46f1319874801fb3
Stevan Little <stevan.little@iinteractive.com>
2013-05-13 14:40:34 -0400

    remove $OS_ERROR

--- src/main/scala/org/moe/runtime/builtins/RootEnvironment.scala
+++ src/main/scala/org/moe/runtime/builtins/RootEnvironment.scala
@@ -14,29 +14,14 @@ object RootEnvironment {
     import r.NativeObjects._
 
     env.create("@ARGV", getArray())
-    env.create("%INC",  getHash())
 
     // SEE ALSO -> http://perlcabal.org/syn/S28.html
 
-    env.create("$OS_ERROR", getUndef)
     env.create("$!", getUndef)
 
-    /**
-     * TODO:
-     * we need to be able to alter 
-     * these at runtime and they 
-     * then alter their sources, 
-     * the ENV for any child processes
-     * and the INC to load in modules
-     * this means we will have to 
-     * implement some kind of tied
-     * data structure.
-     * For right now they are basically
-     * just snapshots of these variables
-     * and aren't reflecting correctly
-     * in the runtime.
-     */ 
     env.create("%ENV", getHash(r.getEnv))
+
     env.create("@INC", getArray(r.getIncludeDirs))
+    env.create("%INC",  getHash())
   }
 }
